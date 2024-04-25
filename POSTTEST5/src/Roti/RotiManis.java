4eab018dc597f0e60b41331481f424036df7dfa0
lomeyy <145364699+sefanyaOnibala@users.noreply.github.com>
2024-04-25 21:12:59 +0800

    Add files via upload

---
+++ POSTTEST5/src/Roti/RotiManis.java
@@ -0,0 +1,33 @@
+package Roti;
+
+public class RotiManis extends roti {
+    protected String rasa;
+  
+    public RotiManis(String nama, int harga, int stok, String rasa) {
+      super(nama, harga, stok);
+      this.rasa = rasa;
+    }
+  
+    public String getRasa() {
+      return rasa;
+    }
+  
+    public void setRasa(String rasa) {
+      this.rasa = rasa;
+    }
+  
+    @Override
+    public String getTipeRoti() {
+        return "Manis";
+    }
+
+    @Override
+    public String toString() {
+      return super.toString() + " - Rasa: " + rasa;
+    }
+
+}
+
+
+
+
---
+++ POSTTEST5/src/Roti/roti.java
@@ -0,0 +1,56 @@
+package Roti;
+
+public abstract class roti {
+    protected int id;
+    protected String nama;
+    protected int harga;
+    protected int stok;
+    static int idCounter = 1;
+
+    public roti(String nama, int harga, int stok) {
+        this.id = idCounter++;
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
+
+    //abstract method untuk mendapatkan tipe roti
+    public abstract String getTipeRoti();
+
+    //getter untuk ID (penerapan encapsulation)
+    public int getId() {
+        return id;
+    }
+
+    //getter untuk NAMA
+    public String getNama() {
+        return nama;
+    }
+    //getter untuk Harga
+    public int getHarga() {
+        return harga;
+    }
+
+    //getter untuk stok
+    public int getStok() {
+        return stok;
+    }
+
+    //setter untuk Nama (penerapan encapsulation)
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    //setter untuk Harga
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    //setter untuk stok
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+}
+
+
+
